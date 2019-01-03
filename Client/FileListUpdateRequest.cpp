c528e7caadcaeeaf926fc5a6f8b93c084250203a
Le Duy Khanh <khanhduong1203@gmail.com>
2019-01-03 22:24:54 +0700

    sua goi tin Gửi danh sách file update (client.cpp)

--- Client/FileListUpdateRequest.cpp
+++ Client/FileListUpdateRequest.cpp
@@ -1,31 +1,9 @@
 #include "message.h"
-#include <sys/types.h>
-#include <dirent.h>
-#include <errno.h>
-#include <vector>
-#include <string>
-#include <iostream>
 
 using namespace std;
 
 int FileListUpdateRequest::Write(int sockfd)
 {
-    //list_file
-    string dir = string("\\Share");
-    vector<string> files = vector<string>();
-
-    DIR *folder;
-    struct dirent *d_file;
-    if((folder  = opendir(dir.c_str())) == NULL) {
-        cout << "Error(" << errno << ") opening " << dir << endl;
-        return errno;
-    }
-
-    while ((d_file = readdir(folder)) != NULL) {
-        files.push_back(string(d_file->d_name));
-    }
-    closedir(folder);
-
     // size
     size_t size = sizeof(this->type) + sizeof(this->n_files);
     vector<File> file_list = this->file_list;
